c6d30aef28e71ea81d3e732865f1db4b1b219333
MH.Shanto <mtomking0199@gmail.com>
2020-12-29 03:08:41 +0600

    caffeine with condition

---
+++ FirstClass/src/Mahdi/condition.java
@@ -0,0 +1,32 @@
+package Mahdi;
+
+import java.util.Scanner;
+
+public class condition {
+	
+	static Scanner myScanner=new Scanner(System.in);
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		prln("what is your name :");
+		String myName=myScanner.nextLine();
+		int lnth=myName.length();
+		if(lnth<9) {
+			prln(myName+" This name is not a good name");
+		}else if(lnth>9) {
+			prln(myName+" This name is too long");
+		}else {
+			prln(myName+" You are a lucky persion");
+		}
+		
+	}
+	
+	
+//	created method
+	static void prln(Object anyObject) {
+		System.out.println(anyObject);
+	}
+	static void pr(Object anyObject) {
+		System.out.print(anyObject);
+	}
+}
